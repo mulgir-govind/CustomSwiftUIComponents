9b05006c26f1c9c63e7e81315282201e090f9def
mulgir-govind <govind.mulgir.17@gmail.com>
2025-09-28 16:35:07 +0530

    Add custom TextField with bordered style

---
+++ Sources/CustomSwiftUIComponents/CSCOption.swift
@@ -0,0 +1,12 @@
+//
+//  CSCOption.swift
+//  CustomSwiftUIComponents
+//
+//  Created by Personal on 28/09/25.
+//
+
+import Foundation
+
+protocol CSCOption: Hashable, Identifiable {
+    var optionText: String { get }
+}
---
+++ Sources/CustomSwiftUIComponents/CSCTextField.swift
@@ -0,0 +1,228 @@
+//
+//  SwiftUIView.swift
+//  CustomSwiftUIComponents
+//
+//  Created by Personal on 28/09/25.
+//
+
+import SwiftUI
+
+public enum CSCTextFieldIconPosition {
+    case none
+    case leading
+    case trailing
+}
+
+public struct CSCTextFieldConfig {
+    var font: Font
+    var textColor: Color
+    var placeholderColor: Color
+    var cornerRadius: CGFloat
+    var borderColor: Color
+    var borderWidth: CGFloat
+    var padding: CGFloat
+
+    public init(
+        font: Font = .title3,
+        textColor: Color = .primary,
+        placeholderColor: Color = .secondary,
+        cornerRadius: CGFloat = 12,
+        borderColor: Color = .gray,
+        borderWidth: CGFloat = 2,
+        padding: CGFloat = 12
+    ) {
+        self.font = font
+        self.textColor = textColor
+        self.placeholderColor = placeholderColor
+        self.cornerRadius = cornerRadius
+        self.borderColor = borderColor
+        self.borderWidth = borderWidth
+        self.padding = padding
+    }
+
+    func getBorderColor(for state: CSCTextFieldState) -> Color {
+        switch state {
+            case .unknown: .gray
+            case .valid: .green
+            case .invalid: .red
+        }
+    }
+}
+
+public enum CSCTextFieldState {
+    case unknown
+    case valid
+    case invalid(errorMessage: String?)
+}
+
+private struct CSCTextFieldStyle: ViewModifier {
+    let config: CSCTextFieldConfig
+
+    public func body(content: Content) -> some View {
+        content
+            .font(config.font)
+            .foregroundColor(config.textColor)
+            .padding(config.padding)
+            .background(
+                RoundedRectangle(cornerRadius: config.cornerRadius)
+                    .stroke(config.borderColor, lineWidth: config.borderWidth)
+            )
+    }
+}
+
+private extension View {
+    func cscTextFieldStyle(config: CSCTextFieldConfig) -> some View {
+        self.modifier(CSCTextFieldStyle(config: config))
+    }
+}
+
+public struct CSCTextField: View {
+    @Binding var textValue: String
+    let labelText: String?
+    let placeholderText: String?
+    let systemImageName: String?
+    let iconPosition: CSCTextFieldIconPosition
+    var config: CSCTextFieldConfig
+
+    // Validation closure
+    var validation: ((String) -> CSCTextFieldState)?
+
+    @State private var borderColor: Color
+    @State private var isFirstTimeFocused: Bool = true
+    @State private var currentState: CSCTextFieldState = .unknown
+
+    @FocusState private var isFocused: Bool
+
+    public init(
+        textValue: Binding<String>,
+        labelText: String? = nil,
+        placeholderText: String? = nil,
+        errorMessage: String? = nil,
+        systemImageName: String? = nil,
+        iconPosition: CSCTextFieldIconPosition = .trailing,
+        config: CSCTextFieldConfig = CSCTextFieldConfig(),
+        validation: ((String) -> CSCTextFieldState)? = nil) {
+        self._textValue = textValue
+        self.labelText = labelText
+        self.placeholderText = placeholderText
+        self.systemImageName = systemImageName
+        self.iconPosition = iconPosition
+        self.config = config
+        self.validation = validation
+        self._borderColor = State(initialValue: config.borderColor)
+    }
+
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            if let labelText = labelText, !labelText.isEmpty {
+                Text(labelText)
+                    .font(config.font)
+                    .foregroundColor(config.textColor)
+            }
+
+            HStack {
+                if iconPosition == .leading, let systemImageName {
+                    Image(systemName: systemImageName)
+                        .font(config.font)
+                        .foregroundColor(config.textColor)
+                }
+                
+                TextField(
+                    placeholderText ?? "",
+                    text: $textValue
+                )
+                .textFieldStyle(.plain)
+                .focused($isFocused)
+                .onSubmit { runValidation() }
+                .foregroundColor(config.textColor)
+
+                if iconPosition == .trailing, let systemImageName {
+                    Image(systemName: systemImageName)
+                        .font(config.font)
+                        .foregroundColor(config.textColor)
+                }
+            }
+            .cscTextFieldStyle(config: configWithDynamicBorder())
+            
+            if case .invalid(let errorMessage) = currentState {
+                if let errorMessage {
+                    Text(errorMessage)
+                        .foregroundStyle(.red)
+                }
+            }
+        }
+        .padding(4)
+        .onChange(of: isFocused, { oldValue, newValue in
+            print("GYAMA_test## focused changed")
+            if !isFirstTimeFocused {
+                print("GYAMA_test## firt time execution")
+                runValidation()
+                isFirstTimeFocused.toggle()
+            } else {
+                print("GYAMA_test## next time execution")
+            }
+        })
+        .onChange(of: textValue, { oldValue, newValue in
+            print("GYAMA_test## Value changed")
+            runValidation()
+        })
+    }
+
+    private func runValidation() {
+        if let validation = validation {
+            let newState = validation(textValue)
+
+            let newBorderColor: Color
+            switch newState {
+            case .unknown:
+                newBorderColor = .gray
+                break
+            case .valid:
+                newBorderColor = isFocused ? .green : .gray
+            case .invalid(_):
+                newBorderColor = isFirstTimeFocused ? .gray : .red
+            }
+
+            currentState = newState
+            borderColor = newBorderColor.opacity(0.5)
+        }
+    }
+
+    private func configWithDynamicBorder() -> CSCTextFieldConfig {
+            var updated = config
+            updated.borderColor = borderColor
+            return updated
+        }
+}
+
+#Preview {
+    @Previewable @State var firstName: String = ""
+    @Previewable @State var lastName: String = ""
+
+    ScrollView {
+        CSCTextField(
+            textValue: $firstName,
+            labelText: "First name",
+            placeholderText: "Enter",
+            systemImageName: "person.circle.fill") { value in
+                return value.isEmpty ? .invalid(errorMessage: "First name should be non-empty") : .valid
+            }
+
+        CSCTextField(
+            textValue: $lastName,
+            labelText: "Last name",
+            placeholderText: "Enter",
+            systemImageName: "calendar") { value in
+                return value.isEmpty ? .invalid(errorMessage: "Last name should be non-empty") : .valid
+            }
+        
+        Form {
+            Button {
+                
+            } label: {
+                Text("Save")
+            }
+        }
+    }
+    .padding(18)
+}
